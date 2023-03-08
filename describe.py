ab969216732ba73ddcdbd3889cc6ae704aedab5d
amr <andi@noshut.de>
2023-03-08 18:24:28 +0100

    v.1 mean, std, min, max done

--- describe.py
+++ describe.py
@@ -25,12 +25,46 @@ df = df.drop(df.columns[toDrop], axis=1)
 
 def count(df):
 	count = 0.0
-	print(df[20])
 	for i in range(len(df)):
 		if not math.isnan(df[i]):
 			count += 1
 	return count
 
+def mean(df, count):
+	sum = 0.0
+	for i in range(len(df)):
+		if not math.isnan(df[i]):
+			sum += df[i]
+	return (sum / count)
+	
+def std(df, count, mean):
+	sum = 0.0
+	for i in range(len(df)):
+		if not math.isnan(df[i]):
+			sum += (df[i] - mean) ** 2
+	return (math.sqrt(sum/(count - 1)))
+
+def min(df):
+	min = df[0]
+	for i in range(len(df)):
+		if not math.isnan(df[i]):
+			if min > df[i]:
+				min = df[i]
+	return min
+
+def max(df):
+	max = df[0]
+	for i in range(len(df)):
+		if not math.isnan(df[i]):
+			if max < df[i]:
+				max = df[i]
+	return max
+
+def perc(df, mod, count, max):
+	perc = mod / 100 * (count + 1)
+	print(perc)
+	return perc
+	
 
 data = np.full((8, 14), 0.0)
 output = pd.DataFrame(data, columns=df.columns, index=['count', 'mean', 'std', 'min', '25%', '50%', '75%', 'max'])
@@ -38,6 +72,13 @@ countOfFeatures = len(df.columns)
 for i in range(countOfFeatures):
 	colName = df.columns[i]
 	output[colName]['count'] = count(df[colName])
+	output[colName]['mean'] = mean(df[colName], output[colName]['count'])
+	output[colName]['std'] = std(df[colName], output[colName]['count'], output[colName]['mean'])
+	output[colName]['min'] = min(df[colName])
+	output[colName]['max'] = max(df[colName])
+	output[colName]['25%'] = perc(df[colName], 0.25, output[colName]['count'], output[colName]['max'])
+	# output[colName]['50%'] = perc(df[colName], 0.50, output[colName]['count'], output[colName]['max'])
+	# output[colName]['75%'] = perc(df[colName], 0.75, output[colName]['count'], output[colName]['max'])
 
 
 # print(df["Astronomy"].count())
@@ -45,6 +86,7 @@ for i in range(countOfFeatures):
 print(input.describe())
 # print(len(df))
 # print(len(df.columns))
+pd.options.display.float_format = "{:.6f}".format
 print(output)
 tableInfo = "\n[" + str(len(output)) + " rows x " + str(len(output.columns)) + " columns]"
 print(tableInfo)
