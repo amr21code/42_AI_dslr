da752c7c3b7be158675573bfe736308eb56066ff
amr <andi@noshut.de>
2023-03-11 17:18:33 +0100

    v.1 percentiles done

--- describe.py
+++ describe.py
@@ -10,8 +10,13 @@ from pandas.api.types import is_numeric_dtype
 # except:
 # 	print("no valid dataset supplied")
 
+try:
+	file = sys.argv[1]
+	input = pd.read_csv(file)
+except:
+	print("file access error - please specify valid dataset.csv")
+	sys.exit(1)
 
-input = pd.read_csv('datasets/dataset_train.csv')
 df = input
 
 countOfFeatures = len(df.columns)
@@ -60,13 +65,26 @@ def max(df):
 				max = df[i]
 	return max
 
-def perc(df, mod, count, max):
-	perc = sorted[round(mod * (count + 1))]
-	# print(perc1)
-	# perc2 = df[int(mod * (count + 1)) - 1]
-	# print(perc2)
-	# weight = (perc1 - perc2) * mod
-	# print(weight)
+def perc(df, mod, count):
+	# sorted = df.copy()
+	sorted = df.sort_values(ignore_index=True)
+	# print(df[round(mod * (count + 1))])
+	# print(sorted[1175:1185])
+	idx = mod * (count)
+	# if (mod * (count - 1)).is_integer():
+	# 	return sorted[round(idx)]
+	idx = round(idx)
+	# print(idx)
+	perc_high = sorted[idx]
+	# print(perc_high)
+	perc_low = sorted[idx - 1]
+	# print(perc_low)
+	fract = mod * (count - 1) - int(mod * (count - 1))
+	if fract == 0.0:
+		return sorted[(mod * (count - 1))]
+	# print(fract)
+	perc = perc_low + (perc_high - perc_low) * fract
+	# print(perc)
 	# perc = (perc1 + perc2) / 2 + weight
 	return perc
 	
@@ -81,10 +99,9 @@ for i in range(countOfFeatures):
 	output[colName]['std'] = std(df[colName], output[colName]['count'], output[colName]['mean'])
 	output[colName]['min'] = min(df[colName])
 	output[colName]['max'] = max(df[colName])
-	output[colName]['25%'] = perc(df[colName], 0.25, output[colName]['count'], output[colName]['max'])
-	# output[colName]['50%'] = perc(df[colName], 0.50, output[colName]['count'], output[colName]['max'])
-	# output[colName]['75%'] = perc(df[colName], 0.75, output[colName]['count'], output[colName]['max'])
-
+	output[colName]['25%'] = perc(df[colName], 0.25, output[colName]['count'])
+	output[colName]['50%'] = perc(df[colName], 0.50, output[colName]['count'])
+	output[colName]['75%'] = perc(df[colName], 0.75, output[colName]['count'])
 
 # print(df["Astronomy"].count())
 # print(df.head())
